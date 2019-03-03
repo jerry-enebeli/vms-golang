82535806b8dbd5913b222fcb49a268bea689d218
jerry-enebeli <>
2019-03-03 21:10:19 +0100

    updates

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
-go build -o vms
+GOARCH=amd64 GOOS=linux go build -o vms
 
 docker build -t vms .
 
-echo done
+docker run -d vms
+echo vms server running as a docker container
+echo ------------------------------------------
+echo 'type docker ps to view container'
--- dockerfile
+++ dockerfile
@@ -2,8 +2,13 @@ FROM alpine
 
 RUN apk add ca-certificates
 
+RUN mkdir config
+
+COPY config/env.json config
+
 COPY vms /
 
-EXPOSE 3500
+EXPOSE 8081
+EXPOSE 8082
 
 ENTRYPOINT ["/vms"]
